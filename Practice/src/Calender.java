c15e27d79ff0a7dfdb2241b7cf0cff83205bb346
Seock <whdduqqnld@gmail.com>
2023-07-13 18:52:15 +0900

    second--version

--- Practice/src/Calender.java
+++ Practice/src/Calender.java
@@ -1,6 +1,12 @@
 public class Calender {
     public static void main(String[] args) {
-        System.out.println("Hello, Calender!");
-        
+        System.out.println("일\t월\t화\t수\t목\t금\t토\n");
+        for(int i=1;i<29;i++){
+            System.out.print(i+"\t");
+            if(i%7==0){
+                System.out.println("\n");
+            }
+        }
+
     }
 }
